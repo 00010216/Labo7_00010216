3db2918c4f138cae1a85b7915ff5dc4238064735
00010216 <00010216@uca.edu.sv>
2018-05-27 15:26:48 -0600

    se agrego fragment manager para abrir el log in fragment

--- app/src/main/java/com/example/kcruz/room/MainActivity.java
+++ app/src/main/java/com/example/kcruz/room/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.kcruz.room;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.kcruz.room.fragments.LogInFragment;
+
+public class MainActivity extends AppCompatActivity implements LogInFragment.LoginClickListener{
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_log_in);
+        setContentView(R.layout.activity_main);
 
+        LogInFragment frag = new LogInFragment();
+        //frag.setArguments(bundle);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.frameLayout, frag);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onLogInClick(View v) {
 
     }
 }
