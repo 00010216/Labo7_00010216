efa1f9c4c28ed393d3723e94a237afd8871caa9a
00010216 <00010216@uca.edu.sv>
2018-05-27 15:59:11 -0600

    se creo fragmento para el log in y interfaz para darle funcionalidad al onclick

--- app/src/main/java/com/example/kcruz/room/fragments/LogInFragment.java
+++ app/src/main/java/com/example/kcruz/room/fragments/LogInFragment.java
@@ -1,4 +1,45 @@
 package com.example.kcruz.room.fragments;
 
-public class LogInFragment {
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.kcruz.room.R;
+
+import java.util.List;
+
+public class LogInFragment extends android.support.v4.app.Fragment {
+    Activity activity;
+    Button login;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = getActivity();
+
+    }
+
+    public interface LoginClickListener{
+        public void onLogInClick(View v);
+    }
+
+    LoginClickListener mlistener;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_log_in,container,false);
+        login = view.findViewById(R.id.btn_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            mlistener.onLogInClick(v);
+            }
+        });
+        return view;
+    }
 }
---
+++ app/src/main/java/com/example/kcruz/room/fragments/Prueba.java
@@ -0,0 +1,4 @@
+package com.example.kcruz.room.fragments;
+
+public class Prueba {
+}
