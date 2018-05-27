dc4cf5b50ab94242326179d34de624855fe02fca
00010216 <00010216@uca.edu.sv>
2018-05-27 17:31:13 -0600

    se agrego funcionalidad al drawer

--- app/src/main/java/com/example/kcruz/room/fragments/DrawerFragment.java
+++ app/src/main/java/com/example/kcruz/room/fragments/DrawerFragment.java
@@ -1,21 +1,69 @@
 package com.example.kcruz.room.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.kcruz.room.R;
 
-public class Prueba extends Fragment {
+public class DrawerFragment extends Fragment {
+    private DrawerLayout drawer;
+    Activity activity;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = getActivity();
+
+    }
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_notes,container,false);
+        final View view = inflater.inflate(R.layout.fragment_drawer,container,false);
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        drawer = view.findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(activity, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        NavigationView navigationView = view.findViewById(R.id.navigationView);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                item.setChecked(true);
+
+                drawer.closeDrawers();
+
+                switch(item.getItemId()){
+                }
+
+                DrawerLayout drawer = (DrawerLayout) view.findViewById(R.id.drawerLayout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
 
         return view;
     }
+
 }
