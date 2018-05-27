7d7d2ef6db217e03e0ed4889b909589a6dca17b7
00010216 <00010216@uca.edu.sv>
2018-05-27 16:03:25 -0600

    fragmento de prueba que se abre al dar click login

--- app/src/main/java/com/example/kcruz/room/fragments/Prueba.java
+++ app/src/main/java/com/example/kcruz/room/fragments/Prueba.java
@@ -1,4 +1,21 @@
 package com.example.kcruz.room.fragments;
 
-public class Prueba {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kcruz.room.R;
+
+public class Prueba extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_notes,container,false);
+
+        return view;
+    }
 }
